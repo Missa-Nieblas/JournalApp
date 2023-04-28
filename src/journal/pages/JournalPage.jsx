109b1c9f0f51d062546a8961d6e6670b17984925
Missa <missa_nieblas@hotmail.com>
2023-04-27 23:54:35 -0700

    Se sigui desarrollando al JournalPage, se crearon carpetas components y layout y se crearons las clases respectivas

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,13 +1,14 @@
 
 import { Typography } from "@mui/material"
-
+import { JournalLayout } from '../layout/JournalLayout'
 
 export const JournalPage = () => {
   return (
-    <>
-    
-      <Typography variant="h1">JournalPage</Typography>
-      
-    </>
+    <JournalLayout>
+
+        <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. A deleniti quae, sit optio corrupti tenetur earum est possimus explicabo reprehenderit. Amet maxime omnis eum mollitia illum animi temporibus esse sed.</Typography>
+        {/* Nothing select */}
+        {/*  */}
+    </JournalLayout>
   )
 }
