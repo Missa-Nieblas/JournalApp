32ef804af0ad6584b35f51f94114a51abed2e360
Missa <missa_nieblas@hotmail.com>
2023-05-03 23:12:34 -0700

    Se creo ImageGallerjunto con imagenes con scrol y tambien se añadio el boton de agregar aun sin funcion

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,13 +1,29 @@
+import { IconButton } from '@mui/material'
 import { JournalLayout } from '../layout/JournalLayout'
 import { NoteView, NothingSelectedView } from "../views"
+import { AddOutlined } from '@mui/icons-material'
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
 
        {/*  <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. A deleniti quae, sit optio corrupti tenetur earum est possimus explicabo reprehenderit. Amet maxime omnis eum mollitia illum animi temporibus esse sed.</Typography> */}
-        {/* <NothingSelectedView/> */}
-        <NoteView/>
+        <NothingSelectedView/>
+        {/* <NoteView/> */}
+
+        <IconButton
+          size='large'
+          sx={{
+            color: 'white',
+            backgroundColor: 'error.main',
+            ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
+            position: 'fixed',
+            right: 50,
+            bottom: 50
+          }}
+        >
+          <AddOutlined sx={{fontSize: 30}}/>
+        </IconButton>
     </JournalLayout>
   )
 }
