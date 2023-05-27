4e67c41e83f3c119f5d334f0c79a24fdedcc1408
Missa <missa_nieblas@hotmail.com>
2023-05-26 23:29:31 -0700

    Se inicia la explicacion de desarrollo de estre proyecto a CRUD y se desarrolla mas Firebase

---
+++ src/store/journal/journalSlice.js
@@ -0,0 +1,51 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const journalSlice = createSlice({
+    name: 'journal',
+    initialState: {
+        isSalving: true,
+        messageSaved: '',
+        notes: [],
+        active: null,
+        //active: {
+            //id:'ABC123',
+            //title: '',
+            //body: '',
+            //date: 1234567,
+            //imageUrls: [],
+        //}
+    },
+    reducers: {
+            addNewEmptyNote: (state, action) => {
+
+            },
+            setActiveNote: (state, action) => {
+
+            },
+            setNotes: (state, action) => {
+
+            },
+            setSaving: (state) => {
+
+            },
+            updateNotes: (state, action) => {
+
+            },
+            deleteNodeById: (state, action) => {
+
+            },
+        },
+    }
+);
+
+
+// Action creators are generated for each case reducer function
+export const { 
+    addNewEmptyNote,
+    setActiveNote,
+    setNotes,
+    setSaving,
+    updateNotes,
+    deleteNodeById,
+
+     } = journalSlice.actions;
